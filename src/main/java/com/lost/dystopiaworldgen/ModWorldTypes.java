727af6103632c2394beee66ba58ee767e5955b56
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-04-26 17:37:19 +1000

    add snowy wasteland single biome world type

--- src/main/java/com/lost/dystopiaworldgen/ModWorldTypes.java
+++ src/main/java/com/lost/dystopiaworldgen/ModWorldTypes.java
@@ -7,4 +7,6 @@ import net.minecraft.world.WorldType;
 public class ModWorldTypes {
 	//custome world type, single biome uses SandyWasteland biome
 	public static final WorldType SinBioSandyWasteland = new WorldTypeSingleBiome("sbsanwas", ModBiomes.sandy_wasteland);
+	//single biome snowy wasteland
+	public static final WorldType SinBioSnowyWasteland = new WorldTypeSingleBiome("sbsnowas", ModBiomes.snowy_wasteland);
 }
