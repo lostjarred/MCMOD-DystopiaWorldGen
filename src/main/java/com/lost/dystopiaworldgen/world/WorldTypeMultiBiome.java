218224c115ec92ad82cfc461d5428eddbecb1400
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-05-27 10:20:46 +1000

    Create WorldTypeMultiBiome.java

---
+++ src/main/java/com/lost/dystopiaworldgen/world/WorldTypeMultiBiome.java
@@ -0,0 +1,11 @@
+package com.lost.dystopiaworldgen.world;
+
+import net.minecraft.world.WorldType;
+
+public class WorldTypeMultiBiome extends WorldType {
+
+	public WorldTypeMultiBiome(String name) {
+		super(name);
+	}
+
+}
