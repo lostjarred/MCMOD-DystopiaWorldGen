6f37456f7fdabd885877572f9ba844020f863519
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-06-18 12:29:26 +1000

    Create BiomeWasteHills.java

---
+++ src/main/java/com/lost/dystopiaworldgen/world/biome/multi/BiomeWasteHills.java
@@ -0,0 +1,12 @@
+package com.lost.dystopiaworldgen.world.biome.multi;
+
+import com.lost.dystopiaworldgen.world.biome.BiomeBase;
+
+public class BiomeWasteHills extends BiomeBase {
+
+	public BiomeWasteHills(BiomeProperties properties, String name) {
+		super(properties, name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
