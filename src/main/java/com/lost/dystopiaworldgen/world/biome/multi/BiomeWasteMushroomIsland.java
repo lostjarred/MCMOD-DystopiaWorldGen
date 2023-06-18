33933b829a21bafdbd3542e88101931a184b112a
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-06-18 12:29:33 +1000

    Create BiomeWasteMushroomIsland.java

---
+++ src/main/java/com/lost/dystopiaworldgen/world/biome/multi/BiomeWasteMushroomIsland.java
@@ -0,0 +1,12 @@
+package com.lost.dystopiaworldgen.world.biome.multi;
+
+import com.lost.dystopiaworldgen.world.biome.BiomeBase;
+
+public class BiomeWasteMushroomIsland extends BiomeBase {
+
+	public BiomeWasteMushroomIsland(BiomeProperties properties, String name) {
+		super(properties, name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
