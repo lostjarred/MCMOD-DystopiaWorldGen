06cf85e96eba2439c7eab7e9dc61700531f020a7
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-06-18 12:35:49 +1000

    Create BiomeWasteStoneBeach.java

---
+++ src/main/java/com/lost/dystopiaworldgen/world/biome/multi/BiomeWasteStoneBeach.java
@@ -0,0 +1,12 @@
+package com.lost.dystopiaworldgen.world.biome.multi;
+
+import com.lost.dystopiaworldgen.world.biome.BiomeBase;
+
+public class BiomeWasteStoneBeach extends BiomeBase {
+
+	public BiomeWasteStoneBeach(BiomeProperties properties, String name) {
+		super(properties, name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
