16e928bc22a14d853dc5be9cdf2227d08313de98
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-06-18 12:35:52 +1000

    Create BiomeWasteSwamp.java

---
+++ src/main/java/com/lost/dystopiaworldgen/world/biome/multi/BiomeWasteSwamp.java
@@ -0,0 +1,12 @@
+package com.lost.dystopiaworldgen.world.biome.multi;
+
+import com.lost.dystopiaworldgen.world.biome.BiomeBase;
+
+public class BiomeWasteSwamp extends BiomeBase {
+
+	public BiomeWasteSwamp(BiomeProperties properties, String name) {
+		super(properties, name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
