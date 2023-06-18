ba76324ce15b6b2143c30500b42790d2415685bd
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-06-18 12:35:43 +1000

    Create BiomeWasteSavanna.java

---
+++ src/main/java/com/lost/dystopiaworldgen/world/biome/multi/BiomeWasteSavanna.java
@@ -0,0 +1,12 @@
+package com.lost.dystopiaworldgen.world.biome.multi;
+
+import com.lost.dystopiaworldgen.world.biome.BiomeBase;
+
+public class BiomeWasteSavanna extends BiomeBase {
+
+	public BiomeWasteSavanna(BiomeProperties properties, String name) {
+		super(properties, name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
