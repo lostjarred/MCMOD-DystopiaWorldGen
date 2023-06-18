15cb3a6714c31b1ff3bc539542da8b1e1caa9552
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-06-18 12:29:24 +1000

    Create BiomeWasteForrestMutated.java

---
+++ src/main/java/com/lost/dystopiaworldgen/world/biome/multi/BiomeWasteForrestMutated.java
@@ -0,0 +1,12 @@
+package com.lost.dystopiaworldgen.world.biome.multi;
+
+import com.lost.dystopiaworldgen.world.biome.BiomeBase;
+
+public class BiomeWasteForrestMutated extends BiomeBase {
+
+	public BiomeWasteForrestMutated(BiomeProperties properties, String name) {
+		super(properties, name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
