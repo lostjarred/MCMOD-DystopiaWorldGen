a618c0fb6bee727db6bc7b2a719442d055ef821b
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-08-12 09:42:32 +1000

    add taiga

--- src/main/java/com/lost/dystopiaworldgen/ModBiomes.java
+++ src/main/java/com/lost/dystopiaworldgen/ModBiomes.java
@@ -5,6 +5,7 @@ import com.lost.dystopiaworldgen.world.biome.multi.BiomeWasteForrest;
 import com.lost.dystopiaworldgen.world.biome.multi.BiomeWasteHills;
 import com.lost.dystopiaworldgen.world.biome.multi.BiomeWasteOcean;
 import com.lost.dystopiaworldgen.world.biome.multi.BiomeWastePlains;
+import com.lost.dystopiaworldgen.world.biome.multi.BiomeWasteTaiga;
 import com.lost.dystopiaworldgen.world.biome.single.BiomeSandyWasteland;
 import com.lost.dystopiaworldgen.world.biome.single.BiomeSnowyWasteland;
 
@@ -62,6 +63,12 @@ public class ModBiomes {
 			.setTemperature(0.7F)
 			.setRainfall(0.8F), 
 			"wasteforrest");
+	
+	public static BiomeWasteTaiga waste_taiga = new BiomeWasteTaiga(new BiomeProperties("Waste Taiga")
+			.setBaseHeight(0.2F)
+			.setHeightVariation(0.2F)
+			.setTemperature(0.25F)
+			.setRainfall(0.8F), "wastetaiga");
 	
 	public static void register(IForgeRegistry<Biome> event) {
 		//single biome gen biomes
