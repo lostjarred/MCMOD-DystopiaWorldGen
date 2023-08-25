ba3c8d92eb2ae735bacc473b64ca1cecc91aaeb7
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-08-26 08:13:54 +1000

    Update ModFluid.java

--- src/main/java/com/lost/dystopiaworldgen/fluid/ModFluid.java
+++ src/main/java/com/lost/dystopiaworldgen/fluid/ModFluid.java
@@ -1,10 +1,16 @@
 package com.lost.dystopiaworldgen.fluid;
 
+import java.awt.Color;
+
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fluids.Fluid;
 
 public class ModFluid extends Fluid {
 	public ModFluid(String fluidName, ResourceLocation still, ResourceLocation flowing) {
 		super(fluidName, still, flowing);
+	}
+	
+	public ModFluid(String fluidName, ResourceLocation still, ResourceLocation flowing, Color color){
+		super(fluidName, still, flowing, color);
 	}
 }
