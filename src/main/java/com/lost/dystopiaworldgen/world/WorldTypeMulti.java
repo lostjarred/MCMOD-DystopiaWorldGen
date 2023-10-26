807e730069c71848ad97f82c44f8c5361e97c745
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-10-27 08:20:40 +1100

    add multi biome world type

---
+++ src/main/java/com/lost/dystopiaworldgen/world/WorldTypeMulti.java
@@ -0,0 +1,20 @@
+package com.lost.dystopiaworldgen.world;
+
+import com.lost.dystopiaworldgen.world.genlayer.GenLayerBiomeCustom;
+
+import net.minecraft.world.WorldType;
+import net.minecraft.world.gen.ChunkGeneratorSettings;
+import net.minecraft.world.gen.layer.GenLayer;
+
+public class WorldTypeMulti extends WorldType {
+
+	public WorldTypeMulti(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public GenLayer getBiomeLayer(long worldSeed, GenLayer parentLayer, ChunkGeneratorSettings chunkSettings) {
+		return new GenLayerBiomeCustom(worldSeed);
+	}
+}
