a103a802e69aa9b79fed691faf48f6fdf8e50b43
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-10-27 08:20:59 +1100

    add multi biome gen world type

--- src/main/java/com/lost/dystopiaworldgen/ModWorldTypes.java
+++ src/main/java/com/lost/dystopiaworldgen/ModWorldTypes.java
@@ -1,5 +1,6 @@
 package com.lost.dystopiaworldgen;
 
+import com.lost.dystopiaworldgen.world.WorldTypeMulti;
 import com.lost.dystopiaworldgen.world.WorldTypeSingleBiome;
 
 import net.minecraft.world.WorldType;
@@ -9,4 +10,6 @@ public class ModWorldTypes {
 	public static final WorldType SinBioSandyWasteland = new WorldTypeSingleBiome("sbsanwas", ModBiomes.sandy_wasteland);
 	//single biome snowy wasteland
 	public static final WorldType SinBioSnowyWasteland = new WorldTypeSingleBiome("sbsnowas", ModBiomes.snowy_wasteland);
+	
+	public static final WorldType MulBioSandyWasteland = new WorldTypeMulti("mbsanwas");
 }
