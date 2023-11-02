e20bddec843a1cb782d75f0285668d4acbddf5c4
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-11-03 08:17:39 +1100

    add event handler for water lake gen

--- src/main/java/com/lost/dystopiaworldgen/EventHandler.java
+++ src/main/java/com/lost/dystopiaworldgen/EventHandler.java
@@ -1,8 +1,17 @@
 package com.lost.dystopiaworldgen;
 
+import net.minecraftforge.event.terraingen.PopulateChunkEvent.Populate;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod.EventBusSubscriber
 public class EventHandler {
+	
+	@SubscribeEvent
+	public static void onEvent(Populate event) {
+		if (event.getType() == Populate.EventType.LAKE) {
+			System.out.println("a lake has just generate and I have detected it !");
+		}
+	}
 	
 }
