b325213cb986d2527ced5b6638d260f2df43d221
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-11-03 09:31:55 +1100

    remove unneeded parameters

--- src/main/java/com/lost/dystopiaworldgen/world/biome/BiomeSandyWasteland.java
+++ src/main/java/com/lost/dystopiaworldgen/world/biome/BiomeSandyWasteland.java
@@ -10,10 +10,7 @@ public class BiomeSandyWasteland extends BiomeBase {
 		super(properties, name);
 		this.topBlock = Blocks.SAND.getDefaultState();
         this.fillerBlock = Blocks.SAND.getDefaultState();
-        this.decorator.treesPerChunk = -999;
         this.decorator.deadBushPerChunk = 2;
-        this.decorator.reedsPerChunk = -999;
-        this.decorator.cactiPerChunk = -999;
         this.spawnableMonsterList.add(new Biome.SpawnListEntry(EntityHusk.class, 80, 4, 4));
 	}
 
