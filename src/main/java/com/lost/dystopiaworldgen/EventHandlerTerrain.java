f1c4c170cd3037df6d9346f0a1d2cd7788af8437
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-11-03 19:12:28 +1100

    add check for my worldtype and if lake

--- src/main/java/com/lost/dystopiaworldgen/EventHandlerTerrain.java
+++ src/main/java/com/lost/dystopiaworldgen/EventHandlerTerrain.java
@@ -6,6 +6,11 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class EventHandlerTerrain {
 	@SubscribeEvent
 	public void onEvent(PopulateChunkEvent.Populate event) {
-		System.out.println(">>>> a populate event has occured and I have found it");
+		//System.out.println(">>>> a populate event has occured and I have found it");
+		if (event.getWorld().getWorldType() == ModWorldTypes.MulBioSandyWasteland) {
+			if (event.getType() == PopulateChunkEvent.Populate.EventType.LAKE) {
+				System.out.println(">>>> a lake generation event has occured and I have found it");
+			}
+		}
 	}
 }
