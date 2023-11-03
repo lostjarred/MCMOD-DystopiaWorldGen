3f164c752ecb2a3ce26c8fb187ef492fa31d2f6d
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-11-04 07:53:43 +1100

    redo lake gen

--- src/main/java/com/lost/dystopiaworldgen/EventHandlerTerrain.java
+++ src/main/java/com/lost/dystopiaworldgen/EventHandlerTerrain.java
@@ -1,6 +1,7 @@
 package com.lost.dystopiaworldgen;
 
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.gen.feature.WorldGenLakes;
 import net.minecraftforge.event.terraingen.PopulateChunkEvent;
 import net.minecraftforge.fml.common.eventhandler.Event;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -11,11 +12,11 @@ public class EventHandlerTerrain {
 		//System.out.println(">>>> a populate event has occured and I have found it");
 		if (event.getWorld().getWorldType() == ModWorldTypes.MulBioSandyWasteland) {
 			if (event.getType() == PopulateChunkEvent.Populate.EventType.LAKE) {
-				int blockposx = event.getChunkX() * 16;
-				int blockposz = event.getChunkZ() * 16;
-				int blockposy = 90;
-				System.out.println(">>>> a lake generation event has occured and I have found it " + blockposx + " " + blockposy + " "+ blockposz);
-				ModWorldGen.LAKE_CONTAINMENTEDWATER.generate(event.getWorld(), event.getRand(), new BlockPos( blockposx, 90, blockposz ) );
+				int blockposx = (event.getChunkX() * 16) + 8;
+				int blockposz = (event.getChunkZ() * 16) + 8;
+				int blockposy = 256;
+				//System.out.println(">>>> a lake generation event has occured and I have found it " + blockposx + " " + blockposy + " "+ blockposz);
+				(new WorldGenLakes(ModBlocks.containmentedwaterfluidblock)).generate(event.getWorld(), event.getRand(), new BlockPos(blockposx, blockposy, blockposz));
 				event.setResult(Event.Result.DENY);
 			}
 		}
