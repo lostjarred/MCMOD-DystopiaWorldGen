e273699ef59ace630ceff03a21ba0aa188c2e5ce
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-11-03 19:33:53 +1100

    add containmented water lake

---
+++ src/main/java/com/lost/dystopiaworldgen/ModWorldGen.java
@@ -0,0 +1,7 @@
+package com.lost.dystopiaworldgen;
+
+import net.minecraft.world.gen.feature.WorldGenLakes;
+
+public class ModWorldGen {
+	public static WorldGenLakes LAKE_CONTAINMENTEDWATER = new WorldGenLakes(ModBlocks.containmentedwaterfluidblock);
+}
