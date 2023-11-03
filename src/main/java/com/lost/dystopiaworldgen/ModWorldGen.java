b70a8c3683ba0f667364f63569c7c4074eecf28a
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-11-04 07:53:54 +1100

    no longer used

--- src/main/java/com/lost/dystopiaworldgen/ModWorldGen.java
+++
@@ -1,7 +0,0 @@
-package com.lost.dystopiaworldgen;
-
-import net.minecraft.world.gen.feature.WorldGenLakes;
-
-public class ModWorldGen {
-	public static WorldGenLakes LAKE_CONTAINMENTEDWATER = new WorldGenLakes(ModBlocks.containmentedwaterfluidblock);
-}
