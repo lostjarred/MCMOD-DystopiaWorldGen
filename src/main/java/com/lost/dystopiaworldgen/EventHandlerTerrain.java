cc3823311c73f8341f552c64bdfc355b0e7d9532
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-11-03 19:40:15 +1100

    attempt to replace water lakes..

--- src/main/java/com/lost/dystopiaworldgen/EventHandlerTerrain.java
+++ src/main/java/com/lost/dystopiaworldgen/EventHandlerTerrain.java
@@ -1,6 +1,8 @@
 package com.lost.dystopiaworldgen;
 
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.event.terraingen.PopulateChunkEvent;
+import net.minecraftforge.fml.common.eventhandler.Event;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class EventHandlerTerrain {
@@ -9,7 +11,11 @@ public class EventHandlerTerrain {
 		//System.out.println(">>>> a populate event has occured and I have found it");
 		if (event.getWorld().getWorldType() == ModWorldTypes.MulBioSandyWasteland) {
 			if (event.getType() == PopulateChunkEvent.Populate.EventType.LAKE) {
-				System.out.println(">>>> a lake generation event has occured and I have found it");
+				int blockposx = event.getChunkX() * 16;
+				int blockposz = event.getChunkZ() * 16;
+				System.out.println(">>>> a lake generation event has occured and I have found it " + blockposx + " " + blockposz);
+				ModWorldGen.LAKE_CONTAINMENTEDWATER.generate(event.getWorld(), event.getRand(), new BlockPos( (event.getChunkX() * 16), 0, (event.getChunkZ() * 16) ) );
+				event.setResult(Event.Result.DENY);
 			}
 		}
 	}
