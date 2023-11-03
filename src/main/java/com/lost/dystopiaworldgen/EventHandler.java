59dd0adc2bd40387db916a22eabf474aab68d7d6
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-11-03 19:07:31 +1100

    remove debug stuff

--- src/main/java/com/lost/dystopiaworldgen/EventHandler.java
+++ src/main/java/com/lost/dystopiaworldgen/EventHandler.java
@@ -1,11 +1,7 @@
 package com.lost.dystopiaworldgen;
 
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraftforge.event.entity.living.LivingEvent.LivingJumpEvent;
 import net.minecraftforge.event.terraingen.PopulateChunkEvent;
-import net.minecraftforge.event.terraingen.PopulateChunkEvent.Populate;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @EventBusSubscriber(modid = DystopiaWorldGen.MODID)
@@ -13,21 +9,6 @@ public class EventHandler {
 	
 	@SubscribeEvent
 	public static void onEvent(PopulateChunkEvent event) {
-			System.out.println("a chunk populate event has occured and I have found it");
-	}
-
-	//from https://jabelarminecraft.blogspot.com/p/minecraft-forge-172-event-handling.html
-	@SubscribeEvent(priority=EventPriority.HIGHEST, receiveCanceled=true)
-	public static void onEvent(LivingJumpEvent event)
-	{
-	    // DEBUG
-	    if (event.getEntity() instanceof EntityPlayer)
-	    {
-	        System.out.println("Boing");
-	    }
-	        
-	}
-
-
-	
+		
+	}	
 }
