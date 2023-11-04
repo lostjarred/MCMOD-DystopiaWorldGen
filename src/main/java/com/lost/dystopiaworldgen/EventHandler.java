632795708a4bf35c54eade5db1dac7114c1347a4
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-11-04 15:32:27 +1100

    replace water in terrain gen

--- src/main/java/com/lost/dystopiaworldgen/EventHandler.java
+++ src/main/java/com/lost/dystopiaworldgen/EventHandler.java
@@ -1,5 +1,9 @@
 package com.lost.dystopiaworldgen;
 
+import net.minecraft.init.Blocks;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraftforge.event.terraingen.ChunkGeneratorEvent;
 import net.minecraftforge.event.terraingen.PopulateChunkEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -9,6 +13,16 @@ public class EventHandler {
 	
 	@SubscribeEvent
 	public static void onEvent(PopulateChunkEvent event) {
-		
-	}	
+		Chunk pchunk = event.getWorld().getChunk(event.getChunkX(), event.getChunkZ());
+		for (int x = 0; x < 16; x++) {
+			for (int z = 0; z < 16; z++) {
+				for (int y = pchunk.getHeightValue(x, z) - 20; y < pchunk.getHeightValue(x, z) + 1; y++) {
+					if (pchunk.getBlockState(x, y, z).getBlock() == Blocks.WATER) {
+						pchunk.setBlockState(new BlockPos(x, y, z), ModBlocks.containmentedwaterfluidblock.getDefaultState());
+					}
+				}
+			}
+		}
+	}
+	
 }
