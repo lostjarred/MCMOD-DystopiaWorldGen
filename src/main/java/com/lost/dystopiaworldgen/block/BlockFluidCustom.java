e1b4399c8103893e2fb23adde08f8ee52d13784f
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-11-05 16:53:57 +1100

    make fluid act like water

--- src/main/java/com/lost/dystopiaworldgen/block/BlockFluidCustom.java
+++ src/main/java/com/lost/dystopiaworldgen/block/BlockFluidCustom.java
@@ -1,10 +1,17 @@
 package com.lost.dystopiaworldgen.block;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 import com.lost.dystopiaworldgen.DystopiaWorldGen;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.fluids.BlockFluidClassic;
 import net.minecraftforge.fluids.Fluid;
 
@@ -23,4 +30,11 @@ public class BlockFluidCustom extends BlockFluidClassic {
 	public Item createitemblock() {
 		return new ItemBlock(this).setRegistryName(getRegistryName());
 	}
+	
+	// from https://github.com/GregTechCEu/GregTech/blob/master/src/main/java/gregtech/api/fluids/MaterialFluidBlock.java
+	@Nullable
+    @Override
+    public Boolean isEntityInsideMaterial(@Nonnull IBlockAccess world, @Nonnull BlockPos blockpos, @Nonnull IBlockState iblockstate, @Nonnull Entity entity, double yToTest, @Nonnull net.minecraft.block.material.Material materialIn, boolean testingHead) {
+        return materialIn == net.minecraft.block.material.Material.WATER ? true : null;
+    }
 }
