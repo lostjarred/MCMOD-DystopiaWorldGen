0a5a7b54ab70e0b5fe895e7b5ef0dcf6a50d0b39
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-11-06 08:18:52 +1100

    add dammage and poison to containmneted water block

---
+++ src/main/java/com/lost/dystopiaworldgen/block/BlockFluidContainWater.java
@@ -0,0 +1,28 @@
+package com.lost.dystopiaworldgen.block;
+
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.MobEffects;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.fluids.Fluid;
+
+public class BlockFluidContainWater extends BlockFluidCustom {
+
+	public BlockFluidContainWater(Fluid fluid, Material material) {
+		super(fluid, material);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void onEntityCollision(World worldIn, BlockPos pos, IBlockState state, Entity entityIn) {
+		if (entityIn instanceof EntityLivingBase) {
+			entityIn.attackEntityFrom(DamageSource.CACTUS, 1.0f);
+			( (EntityLivingBase) entityIn ).addPotionEffect( new PotionEffect(MobEffects.POISON) );
+		}
+	}
+}
