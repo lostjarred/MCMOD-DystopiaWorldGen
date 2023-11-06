012803bbd32ad78e524918eee2ef1993d535bd57
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-11-07 07:48:57 +1100

    update with new parameter

--- src/main/java/com/lost/dystopiaworldgen/world/WorldTypeMulti.java
+++ src/main/java/com/lost/dystopiaworldgen/world/WorldTypeMulti.java
@@ -1,13 +1,17 @@
 package com.lost.dystopiaworldgen.world;
 
+import com.lost.dystopiaworldgen.ModBiomes;
 import com.lost.dystopiaworldgen.world.gen.layer.GenLayerBiomeCustom;
 
 import net.minecraft.world.WorldType;
+import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.ChunkGeneratorSettings;
 import net.minecraft.world.gen.layer.GenLayer;
 
 public class WorldTypeMulti extends WorldType {
-
+	private Biome[] allowedBiomes = {
+			ModBiomes.sandy_wasteland, ModBiomes.glass_wasteland, ModBiomes.sandy_wasteland_hills
+			};
 	public WorldTypeMulti(String name) {
 		super(name);
 		// TODO Auto-generated constructor stub
@@ -15,6 +19,6 @@ public class WorldTypeMulti extends WorldType {
 	
 	@Override
 	public GenLayer getBiomeLayer(long worldSeed, GenLayer parentLayer, ChunkGeneratorSettings chunkSettings) {
-		return new GenLayerBiomeCustom(worldSeed);
+		return new GenLayerBiomeCustom(worldSeed, allowedBiomes);
 	}
 }
