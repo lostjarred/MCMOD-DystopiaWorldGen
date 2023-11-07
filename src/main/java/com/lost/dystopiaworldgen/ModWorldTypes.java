fb128aedf1c0fe974f915aede7488a101abbcb9b
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-11-07 17:10:12 +1100

    update to use new classes

--- src/main/java/com/lost/dystopiaworldgen/ModWorldTypes.java
+++ src/main/java/com/lost/dystopiaworldgen/ModWorldTypes.java
@@ -3,13 +3,15 @@ package com.lost.dystopiaworldgen;
 import com.lost.dystopiaworldgen.world.WorldTypeMultiWastelandSandy;
 import com.lost.dystopiaworldgen.world.WorldTypeMultiWastelandSnow;
 import com.lost.dystopiaworldgen.world.WorldTypeSingleBiome;
+import com.lost.dystopiaworldgen.world.WorldTypeSingleBiomeWastelandSandy;
+import com.lost.dystopiaworldgen.world.WorldTypeSingleBiomeWastelandSnowy;
 
 import net.minecraft.world.WorldType;
 
 public class ModWorldTypes {
 	//single biome World Types
-		public static final WorldType SinBioSandyWasteland = new WorldTypeSingleBiome("sbsanwas", ModBiomes.sandy_wasteland);
-		public static final WorldType SinBioSnowyWasteland = new WorldTypeSingleBiome("sbsnowas", ModBiomes.snowy_wasteland);
+		public static final WorldType SinBioSandyWasteland = new WorldTypeSingleBiomeWastelandSandy("sbsanwas", ModBiomes.sandy_wasteland);
+		public static final WorldType SinBioSnowyWasteland = new WorldTypeSingleBiomeWastelandSnowy("sbsnowas", ModBiomes.snowy_wasteland);
 	
 	//multi biome world types
 		public static final WorldType MulBioSandyWasteland = new WorldTypeMultiWastelandSandy("mbsanwas");
