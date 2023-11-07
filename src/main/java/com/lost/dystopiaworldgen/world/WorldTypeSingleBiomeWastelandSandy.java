178d73102f601cf09fdb72fd7d53e610d6bf1e02
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-11-07 17:09:27 +1100

    add class for world types

---
+++ src/main/java/com/lost/dystopiaworldgen/world/WorldTypeSingleBiomeWastelandSandy.java
@@ -0,0 +1,10 @@
+package com.lost.dystopiaworldgen.world;
+
+import net.minecraft.world.biome.Biome;
+
+public class WorldTypeSingleBiomeWastelandSandy extends WorldTypeSingleBiome {
+
+	public WorldTypeSingleBiomeWastelandSandy(String name, Biome biome) {
+		super(name, biome);
+	}
+}
---
+++ src/main/java/com/lost/dystopiaworldgen/world/WorldTypeSingleBiomeWastelandSnowy.java
@@ -0,0 +1,11 @@
+package com.lost.dystopiaworldgen.world;
+
+import net.minecraft.world.biome.Biome;
+
+public class WorldTypeSingleBiomeWastelandSnowy extends WorldTypeSingleBiome {
+
+	public WorldTypeSingleBiomeWastelandSnowy(String name, Biome biome) {
+		super(name, biome);
+	}
+
+}
