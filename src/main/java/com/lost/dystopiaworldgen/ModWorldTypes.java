4c75a092f3a3ff77e4fb864d0b46ae1864a39498
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-11-07 16:57:54 +1100

    add snow wasteland mult-biome

--- src/main/java/com/lost/dystopiaworldgen/ModWorldTypes.java
+++ src/main/java/com/lost/dystopiaworldgen/ModWorldTypes.java
@@ -1,15 +1,17 @@
 package com.lost.dystopiaworldgen;
 
 import com.lost.dystopiaworldgen.world.WorldTypeMultiWastelandSandy;
+import com.lost.dystopiaworldgen.world.WorldTypeMultiWastelandSnow;
 import com.lost.dystopiaworldgen.world.WorldTypeSingleBiome;
 
 import net.minecraft.world.WorldType;
 
 public class ModWorldTypes {
-	//custome world type, single biome uses SandyWasteland biome
-	public static final WorldType SinBioSandyWasteland = new WorldTypeSingleBiome("sbsanwas", ModBiomes.sandy_wasteland);
-	//single biome snowy wasteland
-	public static final WorldType SinBioSnowyWasteland = new WorldTypeSingleBiome("sbsnowas", ModBiomes.snowy_wasteland);
+	//single biome World Types
+		public static final WorldType SinBioSandyWasteland = new WorldTypeSingleBiome("sbsanwas", ModBiomes.sandy_wasteland);
+		public static final WorldType SinBioSnowyWasteland = new WorldTypeSingleBiome("sbsnowas", ModBiomes.snowy_wasteland);
 	
-	public static final WorldType MulBioSandyWasteland = new WorldTypeMultiWastelandSandy("mbsanwas");
+	//multi biome world types
+		public static final WorldType MulBioSandyWasteland = new WorldTypeMultiWastelandSandy("mbsanwas");
+		public static final WorldType MulBioSnowWasteland = new WorldTypeMultiWastelandSnow("mbsnowas")
 }
