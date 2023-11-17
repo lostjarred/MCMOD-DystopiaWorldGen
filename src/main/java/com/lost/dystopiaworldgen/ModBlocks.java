b7fc45b0642aef5d9711e3f670ea0230f9ff7afc
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-11-18 09:24:11 +1100

    add glass block

--- src/main/java/com/lost/dystopiaworldgen/ModBlocks.java
+++ src/main/java/com/lost/dystopiaworldgen/ModBlocks.java
@@ -1,28 +1,34 @@
 package com.lost.dystopiaworldgen;
 
+import com.lost.dystopiaworldgen.block.BlockBase;
 import com.lost.dystopiaworldgen.block.BlockFluidContainWater;
 import com.lost.dystopiaworldgen.block.BlockFluidCustom;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.item.Item;
 import net.minecraftforge.registries.IForgeRegistry;
 
 public class ModBlocks {
 	
 	public static BlockFluidContainWater containmentedwaterfluidblock = new BlockFluidContainWater(ModFluids.contaminated_water, ModMaterials.Containainedwater);
+	public static BlockBase glass = new BlockBase(Material.GLASS, "glass");
 	
 	public static void register(IForgeRegistry<Block> registry) {
 		System.out.println("Registering Blocks");
 		registry.register(containmentedwaterfluidblock);
+		registry.register(glass);
 	}
 	
 	public static void registeritemblock(IForgeRegistry<Item> registry) {
 		System.out.println("Registering Item Blocks");
 		registry.register(containmentedwaterfluidblock.createitemblock());
+		registry.register(glass.createItemBlock());
 	}
 	
 	public static void registermodels() {
 		System.out.println("Registering Block Models");
 		containmentedwaterfluidblock.registerItemModel(Item.getItemFromBlock(containmentedwaterfluidblock));
+		glass.registerItemModel(Item.getItemFromBlock(glass));
 	}
 }
