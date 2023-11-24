5d13a017b5f41b5bdf1d28b30f42a1c391df4a3c
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-11-24 17:38:06 +1100

    rename

--- src/main/java/com/lost/dystopiaworldgen/ModBlocks.java
+++ src/main/java/com/lost/dystopiaworldgen/ModBlocks.java
@@ -13,23 +13,23 @@ import net.minecraftforge.registries.IForgeRegistry;
 public class ModBlocks {
 	
 	public static BlockFluidContainWater containmentedwaterfluidblock = new BlockFluidContainWater(ModFluids.contaminated_water, ModMaterials.Containainedwater);
-	public static BlockBase glass = new BlockBase(Material.GLASS, "glass").setCreativeTab(CreativeTabs.MATERIALS);
+	public static final BlockBase DGLASS = new BlockBase(Material.GLASS, "dglass").setCreativeTab(CreativeTabs.MATERIALS);
 	
 	public static void register(IForgeRegistry<Block> registry) {
 		System.out.println("Registering Blocks");
 		registry.register(containmentedwaterfluidblock);
-		registry.register(glass);
+		registry.register(DGLASS);
 	}
 	
 	public static void registeritemblock(IForgeRegistry<Item> registry) {
 		System.out.println("Registering Item Blocks");
 		registry.register(containmentedwaterfluidblock.createitemblock());
-		registry.register(glass.createItemBlock());
+		registry.register(DGLASS.createItemBlock());
 	}
 	
 	public static void registermodels() {
 		System.out.println("Registering Block Models");
 		containmentedwaterfluidblock.registerItemModel(Item.getItemFromBlock(containmentedwaterfluidblock));
-		glass.registerItemModel(Item.getItemFromBlock(glass));
+		DGLASS.registerItemModel(Item.getItemFromBlock(DGLASS));
 	}
 }
