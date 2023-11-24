e322dbf4601b022dfeca59f7e6c938fa5a5f2772
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-11-25 08:10:34 +1100

    return opaque cube false

--- src/main/java/com/lost/dystopiaworldgen/block/BlockGlass.java
+++ src/main/java/com/lost/dystopiaworldgen/block/BlockGlass.java
@@ -1,6 +1,7 @@
 package com.lost.dystopiaworldgen.block;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraftforge.fml.relauncher.Side;
@@ -15,12 +16,17 @@ public class BlockGlass extends BlockBase {
 	@SideOnly(Side.CLIENT)
     public BlockRenderLayer getRenderLayer()
     {
-        return BlockRenderLayer.CUTOUT;
+        return BlockRenderLayer.TRANSLUCENT;
     }
 	
 	@Override
 	public BlockGlass setCreativeTab(CreativeTabs tab) {
 		super.setCreativeTab(tab);
 		return this;
+	}
+	
+	@Override
+	public boolean isOpaqueCube(IBlockState state) {
+		return false;
 	}
 }
